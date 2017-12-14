dc54cd6598beb4ddd68fed841d8067907637b2d6
pedrolssila <pedrolssilva@hotmail.com>
2017-12-14 16:14:14 -0200

    Criado primeiro projeto WPF

---
+++ Wpf_firstApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+namespace Wpf_firstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewMode viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel = (MainWindowViewMode)DataContext;            
+        }
+
+        private void ButtonValidateInput(object sender, RoutedEventArgs e)
+        {
+            viewModel.Num1 = txtnumber1.Text;
+            viewModel.Num2 = txtnumber2.Text;
+
+            viewModel.operation = CheckRadionButton();
+            viewModel.CalculateNumbers();
+            if (!string.IsNullOrEmpty(viewModel.Msg))
+            {
+                txtResult.Text = viewModel.Msg;
+            }
+            else if (viewModel.Result != null)
+            {
+                txtResult.Text = viewModel.Result.ToString();
+            }                            
+        }
+
+        private string CheckRadionButton()
+        {
+            if (sum.IsChecked == true)            
+                return "sum";            
+            else if (sub.IsChecked == true)            
+                return "sub";            
+            else if (div.IsChecked == true)            
+                return "div";            
+            else if (mult.IsChecked == true)            
+                return "mult";            
+            else
+                return string.Empty;
+        }
+    }
+}
---
+++ Wpf_firstApp/MainWindowViewMode.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Wpf_firstApp
+{
+    public class MainWindowViewMode : ViewModelBase
+    {
+        #region Attributes
+        private string num1;
+        private string num2;
+        //private float result = 0;
+        private Nullable<float> result = null;
+        private string msg = string.Empty;
+        internal string operation = string.Empty;
+        #endregion
+
+        #region GetterAndSetters
+        public string Num1
+        {
+            get { return num1; }
+            set
+            {
+                if (num1 != value)
+                {
+                    OnPropertyChanged(nameof(Num1));
+                    num1 = value;
+                }
+            }
+
+        }
+        public string Num2
+        {
+            get { return num2; }
+            set
+            {
+                if (num2 != value)
+                {
+                    OnPropertyChanged(nameof(Num2));
+                    num1 = value;
+                }
+            }
+        }
+        public float? Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+
+        public string Msg
+        {
+            get { return msg; }
+            set { msg = value; }
+        }
+        #endregion
+
+        public MainWindowViewMode()
+        {
+            SetCommand();
+        }
+
+        public ICommand calculateCommand { get; set; } = null;
+
+        private void SetCommand()
+        {
+            calculateCommand = new RelayCommand<object>(btnCalcular);
+        }
+
+        private void btnCalcular(object sender)
+        {
+            // Do something here
+        }
+
+        public void CalculateNumbers()
+        {
+            msg = string.Empty;
+
+            // validate inputs:
+            float number1;
+            if (string.IsNullOrEmpty(num1) || string.IsNullOrWhiteSpace(num1))
+            {
+                msg = "number 1 is empty or has space";
+                return;
+            }
+            else if (!float.TryParse(num1, out number1))
+            {
+                msg = "number 1 invalid";
+                return;
+            }
+
+            float number2;
+            if (string.IsNullOrEmpty(num2) || string.IsNullOrWhiteSpace(num2))
+            {
+                msg = "number 2 is empty or has space";
+                return;
+            }
+            else if (!float.TryParse(num2, out number2))
+            {
+                msg = "number 2 invalid";
+                return;
+            }
+
+            switch (operation)
+            {
+                case "sum":
+                    result = number1 + number2;
+                    break;
+                case "sub":
+                    result = number1 - number2;
+                    break;
+                case "div":
+                    result = number1 / number2;
+                    break;
+                case "mult":
+                    result = number1 * number2;
+                    break;
+            }
+        }
+    }
+
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public ViewModelBase() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
